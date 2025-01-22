c1bb0d02eb4dd70d8badeee3b0fbf6b0608058af
Bruno Gomes <gomesbrunosantos@yahoo.com.br>
2025-01-22 18:21:27 -0300

    criei site agencia BRN

---
+++ reveal.js
@@ -0,0 +1,103 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+revelar.reveal('.efeito-txt-topo',{
+    duration:2000,
+    distance:'90px'
+})
+
+revelar.reveal('.efeito-img-topo',{
+    duration:2000,
+    distance:'90px',
+    delay:500
+})
+
+// TITULOS
+revelar.reveal('.titulo',{
+    duration:2000,
+    distance:'90px'
+})
+
+// ESPECIALIDADES
+revelar.reveal('.efeito-especialidades1',{
+    duration:2000,
+    distance:'90px',
+    delay:500
+})
+
+revelar.reveal('.efeito-especialidades2',{
+    duration:2000,
+    distance:'90px',
+    delay:1000
+})
+
+revelar.reveal('.efeito-especialidades3',{
+    duration:2000,
+    distance:'90px',
+    delay:1500
+})
+
+// SOBRE
+revelar.reveal('.efeito-img-sobre',{
+    duration:2000,
+    distance:'90px',
+    origin:'left',
+    delay:500
+})
+
+revelar.reveal('.efeito-txt-sobre',{
+    duration:2000,
+    distance:'90px',
+    origin:'right',
+    delay:1000
+})
+
+// PORTFOLIO
+revelar.reveal('.efeito-img-port1',{
+    duration:2000,
+    distance:'90px',
+    delay:500
+})
+
+revelar.reveal('.efeito-img-port2',{
+    duration:2000,
+    distance:'90px',
+    delay:1000
+})
+
+revelar.reveal('.efeito-img-port3',{
+    duration:2000,
+    distance:'90px',
+    delay:1500
+})
+
+// FORMULARIO
+revelar.reveal('.efeito-text',{
+    duration:2000,
+    distance:'90px',
+    delay:500
+})
+
+revelar.reveal('.efeito-email',{
+    duration:2000,
+    distance:'90px',
+    delay:1000
+})
+
+revelar.reveal('.efeito-tel',{
+    duration:2000,
+    distance:'90px',
+    delay:1500
+})
+
+revelar.reveal('.efeito-textarea',{
+    duration:2000,
+    distance:'90px',
+    delay:2000
+})
+
+revelar.reveal('.efeito-btn-enviar',{
+    duration:2000,
+    distance:'900px',
+    delay:2500
+})
